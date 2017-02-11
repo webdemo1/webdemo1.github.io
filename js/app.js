46cba3be32d514477c1bf5137bf1389969a40c77
Phạm Sơn Trà <pstrak12@gmail.com>
2017-02-11 18:24:33 +0700

    sdadsa

--- js/app.js
+++ js/app.js
@@ -9,11 +9,11 @@ $(document).ready(function () {
 
         responsive: {
             0: {
-                items: 3,
+                items: 1,
                 nav: false
             },
             600: {
-                items: 5,
+                items: 3,
                 nav: false
             },
             1000: {
