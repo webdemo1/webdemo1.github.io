834ad8d4bf423255ac401b490c9cfc235bc45b21
Phạm Sơn Trà <pstrak12@gmail.com>
2017-02-11 17:48:05 +0700

    ádsadsadsadá

--- js/login.js
+++ js/login.js
@@ -33,9 +33,50 @@ $(function() {
 		//create firebase reference
 		var dbRef = new Firebase("https://popping-inferno-4049.firebaseio.com/");
 		var contactsRef = dbRef.child('contacts');
-		
+		var aboutusRef = dbRef.child('aboutus');
+		var servicesRef = dbRef.child('services');
+		var visionRef = dbRef.child('vision');
+					aboutusRef.on("value", function(snap) {
+							event.preventDefault();
+				snap.forEach(function(childSnap){
+					if(childSnap.val().content != ''){
+						$("#aboutuscontent").html(childSnap.val().content);
+					}
+				});
+			});
+			
+			servicesRef.on("value", function(snap) {
+				event.preventDefault();
+				snap.forEach(function(childSnap){
+					console.log(childSnap.val());
+					$("#servicescontent").html(childSnap.val().content);
+					$("#servicesimg1").attr('src', childSnap.val().img1);
+					$("#servicesimg2").attr('src', childSnap.val().img2);
+					$("#servicesimg3").attr('src', childSnap.val().img3);
+					$("#servicesimg4").attr('src', childSnap.val().img4);
+					$("#servicesimg5").attr('src', childSnap.val().img5);
+					$("#servicesimg6").attr('src', childSnap.val().img6);
+					$("#servicesimg7").attr('src', childSnap.val().img7);
+					$("#servicesimg8").attr('src', childSnap.val().img8);
+					$("#servicesimg9").attr('src', childSnap.val().img9);
+				});
+			});
+			
+			visionRef.on("value", function(snap) {
+				event.preventDefault();
+				snap.forEach(function(childSnap){
+					console.log(childSnap.val());
+					$("#visioncontent1").html(childSnap.val().content1);
+					$("#visioncontent2").html(childSnap.val().content2);
+					$("#visionimg1").attr('src', childSnap.val().img1);
+					$("#visionimg2").attr('src', childSnap.val().img2);
+					$("#visionimg3").attr('src', childSnap.val().img3);
+
+				});
+			});
 		//save contact
 		$(document).ready(function(){
+		
 			$("#submit").click(function(event){
 			  event.preventDefault();
 			  if( document.querySelector('#name').value != '' || document.querySelector('#email').value != '' ){
@@ -46,6 +87,7 @@ $(function() {
 					password: document.querySelector('#password').value,
 				  });
 				  window.location.href = "login.html";
+				  alert("Register Done!");
 			  } else {
 				alert('Please fill atlease name or email!');
 			  }
@@ -53,14 +95,7 @@ $(function() {
 			
 			$("#login").click(function(event){
 				event.preventDefault();
-				var listUsers = null;
-				if(document.querySelector('#namelogin').value == "admin" && document.querySelector('#passwordlogin').value == "admin")
-					{
-						window.location.href = "edit.html";
-					}
 				contactsRef.on("value", function(snap) {
-					console.log(snap);
-				  listUsers = snap.val();
 				  snap.forEach(function(childSnap){
 					if(document.querySelector('#namelogin').value == childSnap.val().name && document.querySelector('#passwordlogin').value == childSnap.val().password)
 					{
@@ -68,6 +103,53 @@ $(function() {
 					}
 				  });
 				});
+			});
+			
+			/* Edit js */
+			$("textarea").jqte();
+			$("#aboutusbutton").click(function(){
+				event.preventDefault();
+				aboutusRef
+				  .push({
+					content: $("#aboutusadmin").val(),
+				  });
+				  
+				  alert("Update Done!");
+			});
+			
+			$("#servicesbutton").click(function(){
+				event.preventDefault();
+				console.log($("#servicesadmin").val());
+				$("#aboutuscontent").html($("#servicesadmin").val());
+				servicesRef
+				  .push({
+					content: $("#servicesadmin").val(),
+					img1: $("#servicesimg1admin").val(),
+					img2: $("#servicesimg2admin").val(),
+					img3: $("#servicesimg3admin").val(),
+					img4: $("#servicesimg4admin").val(),
+					img5: $("#servicesimg5admin").val(),
+					img6: $("#servicesimg6admin").val(),
+					img7: $("#servicesimg7admin").val(),
+					img8: $("#servicesimg8admin").val(),
+					img9: $("#servicesimg9admin").val(),
+				  });
+				  alert("Update Done!");
+			});
+			
+			$("#visionbutton").click(function(){
+				event.preventDefault();
+				console.log($("#visioncontent1admin").val());
+				$("#aboutuscontent").html($("#aboutusadmin").val());
+				visionRef
+				  .push({
+					content1: $("#visioncontent1admin").val(),
+					content2: $("#visioncontent2admin").val(),
+					img1: $("#visionimg1admin").val(),
+					img2: $("#visionimg2admin").val(),
+					img3: $("#visionimg3admin").val(),
+				  });
+				  alert("Update Done!");
 			});
 		});
 });
